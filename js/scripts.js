27e6b49a013f8f4adab7ce24c4ee94973849bd06
Naufal Andriana Putra <mr.naufalandriana@gmail.com>
2024-05-11 01:28:52 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+// Ambil elemen pop-up dan tombol penutup
+
+function showAlert() {
+    alert("this button will redirect you to Gmail to compose a message to (mr.naufalandriana@gmail.com)");
+}
