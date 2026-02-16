af211fe0224ebc5c311a4300be331a706cd00c4f
naufalandriana <144977043+naufalandriana@users.noreply.github.com>
2026-02-16 15:37:24 +0700

    Change background animation to fixed position

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -86,7 +86,7 @@ const Blog = () => {
     <div className="min-h-screen pt-24 pb-20 px-4 sm:px-6 lg:px-8 relative overflow-hidden">
       
       {/* --- BACKGROUND ANIMATION START --- */}
-      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+      <div className="fixed inset-0 overflow-hidden pointer-events-none">
         <div className="absolute top-1/4 -left-10 w-72 h-72 bg-accent/10 rounded-full blur-3xl floating-element"></div>
         <div className="absolute bottom-1/4 -right-10 w-72 h-72 bg-success/10 rounded-full blur-3xl floating-element"></div>
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-accent/5 rounded-full blur-3xl floating-element"></div>
